1d0c19774be64d6eceb8665fca5e8eb1b1d0285d
Axandria Hamilton <amhamilton1@student.fullsail.edu>
2023-09-11 22:07:45 -0400

    final commit pt.2

--- api/models/book.js
+++ api/models/book.js
@@ -5,7 +5,7 @@ const bookSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    class: {
+    author: {
         type: String,
         required: true,
     },
--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ const bookRouter = require('./routes/books.js')
 const DATABASE_URL = process.env.DATABASE_URL;
 
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true })
-const db =mongoose.connection;
+const db = mongoose.connection;
 db.on('error', error => console.error(error))
 db.once('open', () => console.log("Database Connection Established"))
 
